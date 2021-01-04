8fdc2f1f57b65691c419cc4881cdc1d9cf41a123
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-01-04 19:21:36 +0300

    ok second_occurence

--- white_belt/1_week/train_exercise/second_occurence.cpp
+++ white_belt/1_week/train_exercise/second_occurence.cpp
@@ -2,3 +2,33 @@
 // Created by Mahmud Jego on 1/4/21.
 //
 
+#include <iostream>
+
+int main()
+{
+	std::string szStr;
+	int nFlag(0);
+
+	std::cin >> szStr;
+	for (int i = 0; i < szStr.size(); ++i)
+	{
+		if (szStr[i] == 'f')
+		{
+			nFlag++;
+		}
+		if (nFlag == 2)
+		{
+			std::cout << i << std::endl;
+			return (0);
+		}
+	}
+	if (nFlag)
+	{
+		std::cout << -1 << std::endl;
+	}
+	else
+	{
+		std::cout << -2 << std::endl;
+	}
+	return (0);
+}
