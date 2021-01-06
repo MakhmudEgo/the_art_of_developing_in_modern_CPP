04e9322f4f4429af08674fc12696deed76eab684
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-01-06 04:10:40 +0300

    ok bus_stops_3

---
+++ white_belt/2_week/set/bus_stops_3.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Mahmud Jego on 1/6/21.
+//
+
+#include <iostream>
+#include <set>
+#include <map>
+
+int main()
+{
+	int nAmountCmd, nAmountStops, nBus = 1;
+	std::string szStop;
+	std::map<std::set<std::string>, int> mBus;
+	std::set<std::string> sBus;
+
+	std::cin >> nAmountCmd;
+	for (int i = 0; i < nAmountCmd; ++i)
+	{
+		std::cin >> nAmountStops;
+		for (int j = 0; j < nAmountStops; ++j)
+		{
+			std::cin >> szStop;
+			sBus.insert(szStop);
+		}
+		if (mBus.count(sBus))
+		{
+			std::cout << "Already exists for " << mBus[sBus];
+		}
+		else
+		{
+			std::cout << "New bus " << nBus;
+			mBus[sBus] = nBus++;
+		}
+		std::cout << std::endl;
+		sBus.clear();
+	}
+}
