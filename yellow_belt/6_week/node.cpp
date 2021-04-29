1c878ef3751e34e4641beabdd564cd84de0a06d2
MakhmudEgo <makhmudego@yandex.ru>
2021-04-29 09:15:49 +0300

    dev all Nodes

--- yellow_belt/6_week/node.cpp
+++ yellow_belt/6_week/node.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "node.h"
+#include "TComparison.h"
+
+DateComparisonNode::DateComparisonNode(const Comparison &cmp, const Date &date)
+: _comparison(cmp), _date(date) {}
+
+bool DateComparisonNode::Evaluate(const Date &date, const std::string &string) {
+	return TComparison(_date, date, _comparison);
+}
+
+bool EmptyNode::Evaluate(const Date &date, const std::string &string) {
+	return false;
+}
+
+bool EventComparisonNode::Evaluate(const Date &date, const std::string &event) {
+	return TComparison(_event, event, _operation);
+}
+
+EventComparisonNode::EventComparisonNode(const Comparison &cmp, const std::string &value)
+: _operation(cmp), _event(value) {}
+
+bool LogicalOperationNode::Evaluate(const Date &date, const std::string &event) {
+	if(_operation == LogicalOperation::And){
+		return _left->Evaluate(date, event) && _right->Evaluate(date, event);
+	}
+	if(_operation == LogicalOperation::Or){
+		return _left->Evaluate(date, event) || _right->Evaluate(date, event);
+	}
+	return false;
+}
+
+LogicalOperationNode::LogicalOperationNode(const LogicalOperation &log, const std::shared_ptr<Node> &left,
+										   const std::shared_ptr<Node> &right)
+: _left(left), _right(right), _operation(log) {}
+
--- yellow_belt/6_week/node.h
+++ yellow_belt/6_week/node.h
@@ -23,33 +23,44 @@ enum class Comparison {
 
 class Node {
 public:
-	Node() {
-
-	}
-	bool Evaluate(const Date&, const std::string&) {
-		return true;
-	}
+	virtual bool Evaluate(const Date&, const std::string&) = 0;
 };
 
 
 class EmptyNode : public Node {
-
+public:
+	bool Evaluate(const Date &date, const std::string &string) override;
 };
+
 class DateComparisonNode : public Node {
 public:
-	DateComparisonNode(const Comparison& cmp, const Date&) {
+	DateComparisonNode(const Comparison& cmp, const Date&);
 
-	}
+	bool Evaluate(const Date &date, const std::string &string) override;
+
+private:
+	const Comparison _comparison;
+	const Date _date;
 };
+
 class EventComparisonNode : public Node {
 public:
-	EventComparisonNode(const Comparison& cmp, const std::string &value) {
+	EventComparisonNode(const Comparison& cmp, const std::string &value);
 
-	}
+	bool Evaluate(const Date &date, const std::string &string) override;
+
+private:
+	const Comparison _operation;
+	const std::string _event;
 };
+
 class LogicalOperationNode : public Node {
 public:
-	LogicalOperationNode(const LogicalOperation& log, const std::shared_ptr<Node>& left, const std::shared_ptr<Node>& right) {
+	LogicalOperationNode(const LogicalOperation& log, const std::shared_ptr<Node>& left, const std::shared_ptr<Node>& right);
 
-	}
+	bool Evaluate(const Date &date, const std::string &event) override;
+
+private:
+	const std::shared_ptr<Node> _left, _right;
+	const LogicalOperation _operation;
 };
